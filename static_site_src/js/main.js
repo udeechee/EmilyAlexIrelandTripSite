b0786de8e74858c749177bbbe4b532a8d431bba4
Alex Iudice <39865832+udeechee@users.noreply.github.com>
2026-03-07 19:13:30 -0500

    Hide original commits with email address

--- static_site_src/js/main.js
+++ static_site_src/js/main.js
@@ -23,6 +23,17 @@
     //     setInterval(updateCountdown, 60000);
     // }
 
+    // --- Shuffle invitee cards ---
+    var grid = document.querySelector('.invitees-grid');
+    if (grid) {
+        var cards = Array.from(grid.children);
+        for (var i = cards.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            grid.appendChild(cards[j]);
+            cards[j] = cards[i];
+        }
+    }
+
     // --- Packing list persistence ---
     // Saves checkbox state to localStorage so it survives page reloads.
     document.querySelectorAll('.checklist input[type="checkbox"]').forEach(function (cb, i) {
